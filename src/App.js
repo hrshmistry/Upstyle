54dd8123fb10564bfa1f9757e4c7d91eb5547985
Harsh Mistry <hhrshmistry@gmail.com>
2022-04-22 08:07:31 +0530

    added React Router and Compact CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
   );
 }
 
