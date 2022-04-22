2d7f782d6b8328efa72ba390508cb0c30951a161
Harsh Mistry <hhrshmistry@gmail.com>
2022-04-22 09:42:24 +0530

    added home page and populated products

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ProductCard from '../../components/ProductCard/ProductCard'
+import { CartState } from '../../context/Context';
+import "./Home.css"
 
 const Home = () => {
+
+    const { state: { products } } = CartState();
+
     return (
-        <div>Home</div>
+        <div className="home">
+            {products.map(product => {
+                return (<div key={product.key}>
+                    <ProductCard product={product} />
+                </div>)
+            })}
+        </div>
     )
 }
 
